27c7dec6d9877e91d33ccba46d72ae68711fdc4e
Nhidinh91 <142903360+Nhidinh91@users.noreply.github.com>
2023-08-29 13:48:25 +0300

    Add files via upload

---
+++ amirLectureQuize.py
@@ -0,0 +1,4 @@
+Acceleration = float(input("Enter the value of acceleration due to gravity in m/s2 "))
+Mass = float(input("Enter the mass of an object in Kg "))
+Weight = Mass*Acceleration
+print(f"weight of the object: {Weight:5.1f}")
